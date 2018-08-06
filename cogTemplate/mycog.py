3c921ff5257f557cb57d12473bbd190ff0cf6978
JJW <jason.wright.2007@gmail.com>
2018-08-06 16:51:53 -0500

    + Cog template

---
+++ cogTemplate/mycog.py
@@ -0,0 +1,10 @@
+from discord.ext import commands
+
+class Mycog:
+    """My custom cog"""
+
+    @commands.command()
+    async def mycom(self, ctx):
+        """This does stuff!"""
+        # Your code will go here
+        await ctx.send("I can do stuff!")
