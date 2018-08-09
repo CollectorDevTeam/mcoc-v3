197a016c62c29c9c85ab8cd0e4e99f42d57af612
JJW <jason.wright.2007@gmail.com>
2018-08-09 16:37:42 -0500

    removed trailing ()
    .text() is not callable

--- alliancewar/alliancewar.py
+++ alliancewar/alliancewar.py
@@ -7,7 +7,7 @@ ICON_SDF = 'https://raw.githubusercontent.com/JasonJW/mcoc-cogs/master/mcoc/data
 COLLECTOR_ICON='https://raw.githubusercontent.com/JasonJW/mcoc-cogs/master/mcoc/data/cdt_icon.png'
 JPAGS = 'http://www.alliancewar.com'
 PATREON = 'https://patreon.com/collectorbot'
-BOOSTDATA = requests.get('http://www.alliancewar.com/global/ui/js/boosts.json').text()
+BOOSTDATA = requests.get('http://www.alliancewar.com/global/ui/js/boosts.json').text
 BOOSTS = json.loads(BOOSTDATA)
 
 AW_MAP_PATHS={
