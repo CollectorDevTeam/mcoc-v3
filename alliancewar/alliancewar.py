91c5d26c15b091805b818413581933aa40ca7005
JJW <jason.wright.2007@gmail.com>
2018-08-14 13:46:06 -0500

    take spaghetti down

--- alliancewar/alliancewar.py
+++ alliancewar/alliancewar.py
@@ -140,7 +140,7 @@ class AllianceWar:
 
             emSummary.add_field(name='Tile {}',value=', '.join(boostvalues))
         page_list.insert(0,emSummary)
-        await *.menu_start(page_list)
+        await PagesMenu.menu_start(page_list)
 
 
 #####
