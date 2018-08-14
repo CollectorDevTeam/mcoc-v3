489d8bdce3338deb8f5f91e2ad5dd661b8e854e9
JJW <jason.wright.2007@gmail.com>
2018-08-14 13:44:44 -0500

    spaghetti meet wall

--- alliancewar/alliancewar.py
+++ alliancewar/alliancewar.py
@@ -140,7 +140,7 @@ class AllianceWar:
 
             emSummary.add_field(name='Tile {}',value=', '.join(boostvalues))
         page_list.insert(0,emSummary)
-        await PagesMenu.menu_start(PagesMenu, page_list)
+        await *.menu_start(page_list)
 
 
 #####
