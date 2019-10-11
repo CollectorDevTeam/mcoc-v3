d68d433f47d918137c253228a13d3ca3115a810b
JJW <jason.wright.2007@gmail.com>
2019-10-11 13:29:10 -0500

    Colon

--- mcoc/mcoc.py
+++ mcoc/mcoc.py
@@ -2,15 +2,14 @@ from redbot.core import commands
 from redbot.core import Config
 from .common.pages_menu import PagesMenu as Menu
 
-class MCOC()
-
+class MCOC():
+    """A CollectorDevTeam package for Marvel's Contest of Champions"""
     def __init__(self, bot):
         self.bot = bot
 
     @commands.group(pass_context=True, aliases=['champs',])
     async def champ(self, ctx):
-        if ctx.invoked_subcommand is None:
-            await send_cmd_help(ctx)
+        """Champion information"""
 
     @champ.command(name='test')
     async def _champ_test(self, ctx):
