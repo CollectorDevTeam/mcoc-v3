f1641481902628691689f319f261c00c7da4e745
JJW <jason.wright.2007@gmail.com>
2019-10-14 17:06:55 -0500

    bot

--- mcoc/__init__.py
+++ mcoc/__init__.py
@@ -2,6 +2,6 @@ from .cdtdata import CDTDATA
 from .test import Mycog
 
 def setup(bot):
-    bot.add_cog(CDTDATA())
+    bot.add_cog(CDTDATA(bot))
     # bot.add_cog(MCOC()) #load MCOC Cog
     bot.add_cog(Mycog())
