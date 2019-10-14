8d59f427bdb331a20fea9c33054d1ba1c75d9d66
JJW <jason.wright.2007@gmail.com>
2019-10-14 12:23:48 -0500

    Basically starting from scratch

--- mcoc/__init__.py
+++ mcoc/__init__.py
@@ -1,5 +1,7 @@
 from .cdtdata import CDTDATA
+from .test import Mycog
 
 def setup(bot):
-    bot.add_cog(CDTDATA(bot))
+    # bot.add_cog(CDTDATA(bot))
     # bot.add_cog(MCOC()) #load MCOC Cog
+    bot.add_cog(Mycog())
