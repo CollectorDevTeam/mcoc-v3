256016d460da9bfc8526cae738c5fa5d4b12eb6d
JJW <jason.wright.2007@gmail.com>
2020-01-30 11:35:58 -0600

    testing api key retrieval

--- mcoc/gshandler.py
+++ mcoc/gshandler.py
@@ -1,15 +1,25 @@
-# from redbot.core import commands
+from redbot.core import commands
+from redbot.core import Checks
+import pygsheets
 # from redbot.core import Config
 # from .common.pages_menu import PagesMenu as Menu
 #
 # GOOGLECREDENTIALS = ''
 #
-# class GSHandler():
-#     def __init__(self, bot):
-#         self.bot = bot
-#         self.config = Config.get_conf(self, identifier=gshandler2019)
-#
-#     @commands.command()
-#     async def testcom(selfself, ctx):
-#         """Test Command String"""
-#         await ctx.send("I can do stuff")
+class GSHandler(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        # self.config = Config.get_conf(self, identifier=gshandler2019)
+
+
+    @commands.command()
+    async def testapi(self, ctx):
+        """Test Command String"""
+        collectordevteam = await self.bot.get_shared_api_tokens("collectordevteam")
+        await ("Token: "+collectordevteam)
+        try:
+            return pygsheets.authorize(custom_credentials=collectordevteam)
+        except FileNotFoundError:
+            err_msg = 'API token failed to authenticate'
+            await ctx.send(err_msg)
+            raise FileNotFoundError(err_msg)
