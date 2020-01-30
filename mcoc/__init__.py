077c7480c79fd70049bf94afc738d4b0f70e6e6f
JJW <jason.wright.2007@gmail.com>
2020-01-30 11:37:58 -0600

    temporarily making gshandler a cog

--- mcoc/__init__.py
+++ mcoc/__init__.py
@@ -2,9 +2,11 @@
 # from .test import Mycog
 from .mcoc import MCOC
 from .roster import ROSTER
+from .gshandler import GSHandler
 
 def setup(bot):
     # bot.add_cog(CDTDATA())
     # bot.add_cog(MCOC()) #load MCOC Cog
     bot.add_cog(MCOC())
     bot.add_cog(ROSTER())
+    bot.add_cog(GSHandler())
