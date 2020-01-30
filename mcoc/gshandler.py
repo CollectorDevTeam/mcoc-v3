8051b1eabaed256c3158813a3d2123f666ef88c5
JJW <jason.wright.2007@gmail.com>
2020-01-30 12:26:19 -0600

    Testing diagnostic channel feedback

--- mcoc/gshandler.py
+++ mcoc/gshandler.py
@@ -5,21 +5,26 @@ import pygsheets
 #
 # GOOGLECREDENTIALS = ''
 #
+diagnostics = '672501245180772353'
 class GSHandler(commands.Cog):
-    # def __init__(self):
-        # self.config = Config.get_conf(self, identifier=gshandler2019)
+    def __init__(self):
+        pass
+    # self.config = Config.get_conf(self, identifier=gshandler2019)
 
 
     @commands.command()
     async def testapi(self, ctx):
         """Test Command String"""
         collectordevteam = await ctx.bot.get_shared_api_tokens("collectordevteam")
-        await ctx.send("Token: {}".format(collectordevteam))
+        Client = await self.authorize(ctx, collectordevteam)
+        if Client is not None:
+            await ctx.send("Client authenticated")
+
+    async def authorize(self, ctx, token):
+        await ctx.send("Token: {}".format(token))
         try:
-            authentication = pygsheets.authorize(custom_credentials=collectordevteam)
-            await ctx.send(authentication)
-            # return pygsheets.authorize(custom_credentials=collectordevteam)
+            return pygsheets.authorize(custom_credentials=token)
         except FileNotFoundError:
             err_msg = 'API token failed to authenticate'
-            await ctx.send(err_msg)
+            await ctx.send(content=err_msg, channel=ctx.bot.get_channel(diagnostics))
             raise FileNotFoundError(err_msg)
