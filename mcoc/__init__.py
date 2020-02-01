548f507ff0ceed1de48103c8c45b27b47802c4ef
JJW <jason.wright.2007@gmail.com>
2020-01-31 19:22:48 -0600

    mcoc

--- mcoc/__init__.py
+++ mcoc/__init__.py
@@ -2,13 +2,13 @@ from mcoc.CDT import CDT
 from mcoc.roster import ROSTER
 # from .cdtdata import CDTDATA
 # from .gshandler import GSHandler
-# from .core import MCOC
+from mcoc.mcoc import MCOC
 
 
 # from mcoc.gshandler import GSHandler
 
 def setup(bot):
-    # bot.add_cog(MCOC())
+    bot.add_cog(MCOC())
     bot.add_cog(CDT())
     bot.add_cog(ROSTER())
     # bot.add_cog(CDTDATA())
