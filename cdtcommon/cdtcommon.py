1ee6edbd107f7ce759c098040a4b2e013b1acaca
Jason <jason@sdicgm.com>
2020-07-27 19:51:05 -0500

    list role users

--- cdtcommon/cdtcommon.py
+++ cdtcommon/cdtcommon.py
@@ -1,7 +1,9 @@
 import discord
 from redbot.core import commands, checks
 from redbot.core.config import Config
+from redbot.core.utils import menus, chat_formatting
 from .cdtembed import Embed
+
 
 class CdtCommon(commands.Cog):
     """
@@ -19,7 +21,7 @@ class CdtCommon(commands.Cog):
 
     @commands.command(pass_context=True, no_pm=True)
     async def showtopic(self, ctx, channel: discord.TextChannel = None):
-        """Play the Channel Topic in the chat channel."""
+        """Show the Channel Topic in the chat channel as a CDT Embed."""
         if channel is None:
             channel = ctx.message.channel
         topic = channel.topic
@@ -29,3 +31,64 @@ class CdtCommon(commands.Cog):
                                  description=topic)
             data.set_thumbnail(url=ctx.message.guild.icon_url)
             await ctx.send(embed=data)
+
+    @commands.command(pass_context=True, name='list_members', aliases=('list_users',))
+    async def _users_by_role(self, ctx, role: discord.Role, use_alias=True):
+        '''Embed a list of server users by Role'''
+        guild = ctx.message.guild
+        pages = []
+        members = self._list_users(ctx, role, ctx.message.guild)
+        if members is not None:
+            if use_alias is True:
+                ret = '\n'.join('{0.display_name}'.format(m) for m in members)
+            else:
+                ret = '\n'.join('{0.name} [{0.id}]'.format(m) for m in members)
+            pagified = chat_formatting.pagify(ret)
+            # if use_alias:
+            #     ret = '\n'.join([m.display_name for m in members])
+            # else:
+            #     ret = '\n'.join([m.name for m in members])
+            for page in pagified:
+                data = self.Embed.create(ctx, title='{0.name} Role - {1} member(s)'.format(role, len(members)),
+                        description=page)
+                pages.append(data)   
+            if len(pages) == 1:
+                await ctx.send(embed=data)
+            # elif len(pages) > 1:
+            #     await menus.menu(ctx=ctx, pages=pages, controls=self._get_controls(pages))
+
+
+    def _list_users(self, ctx, role: discord.Role, guild: discord.guild):
+        """Given guild and role, return member list"""
+        members = []
+        for member in guild.members:
+            if role in member.roles:
+                members.append(member)
+        if len(members) > 0:
+            return members
+        else:
+            return None
+
+        
+    def _get_controls(self, list: list, export: bool = False):
+        controls = {}
+        if len(list) < 5:
+            controls.update({
+                # "<:arrowsleft:735628703824085004>" : menus.prev_page,
+                "<:arrowleft:735628703610044488>" : menus.prev_page,
+                "<:circlex:735628703530483814>" : menus.close_menu, 
+                "<:arrowright:735628703840600094>" : menus.next_page
+                # "<:arrowsright:735628703609913396>": menus.next_page
+            })
+        elif len(list) >= 5:
+            controls.update({
+                "<:arrowsleft:735628703824085004>" : menus.prev_page,
+                "<:arrowleft:735628703610044488>" : menus.prev_page,
+                "<:circlex:735628703530483814>" : menus.close_menu, 
+                "<:arrowright:735628703840600094>" : menus.next_page,
+                "<:arrowsright:735628703609913396>": menus.next_page
+            })
+        if export is True:
+            controls.update({"<:slow:735197282206482502>": close_menu})
+        return controls
+        
