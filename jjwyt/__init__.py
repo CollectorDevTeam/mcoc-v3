4c585a0bd5aa5e61205f603d7e0f99cfc56a7943
Jason Wright <jason@sdicgm.com>
2021-07-22 14:29:06 -0500

    data statement wasn't me

--- jjwyt/__init__.py
+++ jjwyt/__init__.py
@@ -5,8 +5,8 @@ from redbot.core.bot import Red
 
 from .jjwyt import YouTubeID
 
-with open(Path(__file__).parent / "info.json") as fp:
-    __red_end_user_data_statement__ = json.load(fp)["end_user_data_statement"]
+# with open(Path(__file__).parent / "info.json") as fp:
+#     __red_end_user_data_statement__ = json.load(fp)["end_user_data_statement"]
 
 
 async def setup(bot: Red) -> None:
