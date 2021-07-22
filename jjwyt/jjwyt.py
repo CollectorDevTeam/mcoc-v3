0e17ffa71fad5ce840e12b288fd990d6c5e91b74
Jason Wright <jason@sdicgm.com>
2021-07-22 17:09:01 -0500

    modified status messages add, del

--- jjwyt/jjwyt.py
+++ jjwyt/jjwyt.py
@@ -2,6 +2,7 @@ from typing import Literal
 import re
 
 from cdtcommon.cdtcommon import CdtCommon
+from cdtcommon.cdtembed import Embed
 
 import discord
 from redbot.core import commands
@@ -23,6 +24,8 @@ _config_structure = {
         "subscriber_role": None,
     }
 }
+
+_TITLE="CDT Youtube Identity System :sparkles:"
 
 class YouTubeID(commands.Cog):
     """
@@ -48,14 +51,17 @@ class YouTubeID(commands.Cog):
     #pseudocode command layout
     @commands.group()
     async def ytsubs(self, ctx):
+        data = Embed.create(title=_TITLE)
         if not await self.config.user(ctx.author).youtube_id():
-            await ctx.send("You aren't registered dummy")
+            data.description("Your youtube account is not registered.")
+            await ctx.send(data)
             return
         youtubeid = await self.config.user(ctx.author).youtube_id()
         if youtubeid is not None:
-            await ctx.send("{}".format(youtubeid))
-        else:
-            await ctx.send("Your youtube id is not set.  Get smarter.")
+            data.description("Your registered Youtube ID is ``{}``".format(youtubeid))
+            data.url("https://www.youtube.com/channel/{}".format(youtubeid))
+            await ctx.send(data)
+            return
 
     #yt add id
     # - call confirmation
@@ -63,29 +69,39 @@ class YouTubeID(commands.Cog):
     async def add_youtube_id(self, ctx, youtubeid:str):
         """Include your youtube userid or your channel url"""
         # need to regex out the url stuff
+        data = Embed.create(title=_TITLE)
         youtubeid = regexyt(youtubeid)
         await ctx.send("dbg: youtubeid is {}".format(youtubeid))
         if youtubeid is None:
-            await ctx.send("The youtube regex broke, dummy")
+            data.description("Youtube ID could not be extracted.")
+            await ctx.send(embed=data)
             return
         else:
             answer = await CdtCommon._get_user_confirmation(self, ctx, "Do you want to set ``{}`` as your youtube identity?".format(youtubeid))
             if answer:
                 await self.config.user(ctx.author).youtube_id.set(youtubeid)
+                data.description("Youtube ID set as ``{}``".format(youtubeid))
+                await ctx.send(data)
             else:
-                await ctx.send("Cancelled")
+                data.description("Youtube ID was not recorded.")
+                await ctx.send(data)
 
     #yt delete id
-    @ytsubs.command(name="del")
+    @ytsubs.command(name="delete", aliases=("rm", "del"))
     async def delete_youtube_id(self, ctx):
         """Delete your youtube userid"""
+        data = Embed.create(title=_TITLE)
         # need to regex out the url stuff
-        
+        if not await self.config.user(ctx.author).youtube_id():
+            data.description("You are not registered.")
+            await ctx.send(embed=data)
         answer = await CdtCommon._get_user_confirmation(self, ctx, "Do you want to delete your youtube identity?")
         if answer:
             # await self.config.user(ctx.author).youtube().id.set(None)
             await self.config.user(ctx.author).clear() # not sure if this is right
-            await ctx.send("User data deleted. :sparkles:")
+            data.description("User data deleted.")
+            await ctx.send(embed=data)
+            return
 
     #guildowner/admin yt addsubrole channel + role for subscribers
     # @ytsubs.command(name="addsubrole")
