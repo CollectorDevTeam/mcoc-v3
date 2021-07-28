e7d66a4428e0783b7dba33c9ef7a848f668dd5f0
jjw8071 <jason.wright.2007@gmail.com>
2021-07-28 15:52:39 -0500

    fixy

--- cdtcommon/fetch_data.py
+++ cdtcommon/fetch_data.py
@@ -52,10 +52,10 @@ class FetchData():
         stringlist = kabamjson["strings"]
         strings = {}
         for item in stringlist:
-            for k, v in item:
-                if "vn" in pkg.keys():
-                    vn = pkg["vn"]
-                    if isinstance(vn, int):
+            for k, v in json.loads(item): #load each substring into a dict
+                if "vn" in item:
+                    vn = item["vn"]
+                    if isinstance(vn, int): #unlikely, but they might do it
                         vn = str(vn)
                 else:
                     vn = "0.0.0"
