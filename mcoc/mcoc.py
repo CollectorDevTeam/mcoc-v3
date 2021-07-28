975632dc1fa861537d642a7e6830756d09a9ca27
jjw8071 <jason.wright.2007@gmail.com>
2021-07-27 19:01:25 -0500

    ctx

--- mcoc/mcoc.py
+++ mcoc/mcoc.py
@@ -171,7 +171,7 @@ class Champions(commands.Cog):
                 answer = await CdtCommon.get_user_confirmation(self, ctx, "Would you like to review the http_to_json output?")
                 if answer:
                     pages = chat_formatting.pagify("{}".format(jfile))
-                    await menus.menu(pages=pages, controls=CdtCommon.get_controls())
+                    await menus.menu(ctx, pages=pages, controls=CdtCommon.get_controls())
                 if jfile is None:
                     await ctx.send("aiohttp to json failure, returned None")
                 if jfile is not None:
@@ -180,7 +180,7 @@ class Champions(commands.Cog):
                     answer = await CdtCommon.get_user_confirmation(self, ctx, "Would you like to review the snapshot_conversion?")
                     if answer:
                         pages = chat_formatting.pagify("{}".format(jfile))
-                        await menus.menu(pages=pages, controls=CdtCommon.get_controls())
+                        await menus.menu(ctx, pages=pages, controls=CdtCommon.get_controls())
                     async with self.config.words() as words:
                         words.update(jfile["strings"])
                     async with self.config.snapshots(j) as standard:
