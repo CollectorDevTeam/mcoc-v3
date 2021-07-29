17a50b9148b45fe28f19ea45df040a27514ad2ac
jjw8071 <jason.wright.2007@gmail.com>
2021-07-28 20:13:30 -0500

    try the dictionary interaction

--- mcoc/mcoc.py
+++ mcoc/mcoc.py
@@ -149,17 +149,17 @@ class Champions(commands.Cog):
         """MCOC data purge"""
         if dataset in ("snapshot", "snapshots", "words"):
             answer = await CdtCommon.get_user_confirmation(self, ctx, "Do you want to delete {} dataset?".format(dataset))
-        if answer:
-            answer2 = await CdtCommon.get_user_confirmation(self, ctx, "Are you sure?  This is a delete you moron.")
-            if answer2:
-                if dataset is "snapshot" or dataset is "snapshots":
-                    await self.config.snapshots.clear_all()
-                    async with self.config.snapshots() as snapshots:
-                        await ctx.send("Snapshots should be cleared.  {} keys registered".format(len(snapshots.keys())))
-                elif dataset is "words":
-                    await self.config.words.clear_all()
-                    async with self.config.words() as words:
-                        await ctx.send("Snapshots should be cleared.  {} keys registered".format(len(words.keys())))
+            if answer:
+                answer2 = await CdtCommon.get_user_confirmation(self, ctx, "Are you sure?  This is a delete you moron.")
+                if answer2:
+                    if dataset is "snapshot" or dataset is "snapshots":
+                        async with self.config.snapshots() as snapshots:
+                            snapshots.clear()
+                            await ctx.send("Snapshots should be cleared.  {} keys registered".format(len(snapshots.keys())))
+                    elif dataset is "words":
+                        async with self.config.words() as words:
+                            words.clear()
+                            await ctx.send("Snapshots should be cleared.  {} keys registered".format(len(words.keys())))
         return
     
 
