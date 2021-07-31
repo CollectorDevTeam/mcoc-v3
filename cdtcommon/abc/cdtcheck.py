31aaa1332a88c3672ef8c2ec270d4f575985a27a
JJW8071 <jason.wright.2007@gmail.com>
2021-07-31 13:52:02 -0500

    maybe the discord.Role class wrapper is messing it up

--- cdtcommon/abc/cdtcheck.py
+++ cdtcommon/abc/cdtcheck.py
@@ -37,20 +37,11 @@ class CdtCheck(MixinMeta):
         """Check for privileged role from CDT guild"""
         cdtguild = ctx.bot.get_guild(CDTGUILD)
         member = cdtguild.get_member(ctx.author.id)
-        checkrole = discord.Role(cdtguild.get_role(role_id))
+        checkrole = cdtguild.get_role(role_id)
         result = False
         if member is not None and checkrole in member.roles:
             result = True
         return result, checkrole
-
-    # def get_a_role(ctx, guildid, roleid):
-    #     """With guild.id and role.id find role"""
-    #     guild = ctx.bot.get_guild(guildid)
-    #     if guild is not None:
-    #         role = guild.get_role(roleid)
-    #         if role is not None:
-    #             return role
-    #     return None
         
     def is_collectordevteam():
         """Message caller is CollectorDevTeam"""
